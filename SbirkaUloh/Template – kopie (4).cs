551dd2514c89dd6e56a51028c1ac89963d5daa47
Matěj Straka <xstraka@gymkyjov.cz>
2026-03-20 09:25:18 +0100

    dodelat

--- SbirkaUloh/Template – kopie (4).cs
+++ SbirkaUloh/Template – kopie (4).cs
@@ -5,9 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SbirkaUloh
-{
+{/*
+    Naprogramujte metodu, která na základě kartézských souřadnic dvou bodů v rovině spočítá
+    jejich vzdálenost. S pomocí této funkce vytvořte program, který ze vstupu přečte souřadnice
+    vrcholů trojúhelníka v rovině a spočítá velikosti všech tří jeho stran.
+  */
     internal class Program
     {  
+        public static double Vzdalenost (double x1, double x2, double y1, double y2)
+        {
+
+        }
         static void Main(string[] args)
         {
 
