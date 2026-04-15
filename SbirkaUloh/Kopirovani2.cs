c284ffe803145d3abb2fb2d9c8695563ddd7bfee
Matěj Straka <xstraka@gymkyjov.cz>
2026-04-15 09:24:39 +0200

    fibonacci furt nefunguje bro

---
+++ SbirkaUloh/Kopirovani2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SbirkaUloh
+{/*
+  * Napište program, který zkopíruje obsah souboru stary.txt do souboru novy.txt. Navíc
+  * převede velká písmena na malá, vypustí číslice a místo mezer přejde vždy na nový řádek
+  */
+    internal class Program
+    {  
+        static void Metoda (string Vstup, string Vystup)
+        {
+           
+            using (StreamReader sr = new StreamReader(Vstup))
+            {
+                using (StreamWriter wr = new StreamWriter(Vystup))
+                {
+                    string input;
+                    object result;
+                    while ((input = sr.ReadLine())!= null)
+                    {
+                        result = Regex.Replace(input.ToLower().Replace(' ', '\n'), @"\d", "");
+                        wr.WriteLine(result);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string vstup = @"C:\Users\xstraka\test.txt";
+            string vystup = @"C:\Users\xstraka\testOut.txt";
+
+            Metoda(vstup, vystup);
+        }
+    }
+}
---
+++ SbirkaUloh/VyskytZnaku.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbirkaUloh
+{/*
+  * Spočítejte výskyt zadaného znaku ve vstupním souboru dopis.txt. Výskyt vyjádřete
+    absolutním číslem i v procentech.
+  */
+    internal class Program
+    {  
+        static void Vyskyt(string Cesta, int znak)
+        {
+            int pocitadlo = 0;
+            int pocidadilko = 0;
+            using (StreamReader sr = new StreamReader(Cesta))
+            {
+                int input;
+                while ((input = sr.Read()) != -1)
+                {
+                    if (input == znak)
+                    {
+                        pocidadilko++;
+                    }
+                    pocitadlo++;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine($"\nV textu se znak {(char)znak} objevuje {pocidadilko}x, to je {(((double)pocidadilko/(double)pocitadlo)*100):F2}% z celku ({pocitadlo} znaků)");
+        }
+        static void Main(string[] args)
+        {
+            string cesta = @"C:\Users\xstraka\test.txt";
+            Console.WriteLine("Zadej hledaný znak:");
+            int znak = Console.Read();
+            Vyskyt(cesta, znak);
+        }
+    }
+}
